d0fb9fb41816ac36aadbe04904bb929e9e97fd06
Ahmed Boukadida <ahmedboukadida.ts@gmail.com>
2023-04-17 17:49:48 +0100

    second commit

--- public/app/Models/Post.php
+++ public/app/Models/Post.php
@@ -8,18 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-
-    // protected $table = 'posts';
-
-    // protected $primaryKey = 'title';
-
-    // protected $timestamps = false;
-
-    // protected $dataTime = ''U;
-
-    // protected $connection = 'sqlite';
-
-    // protected $attibutes = [
-    //     'is_published' => 2,
-    // ];
 }
